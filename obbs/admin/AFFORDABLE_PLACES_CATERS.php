23f7860d19723be257d2e2ad24b4f67892a556b9
jatinrawatrawat2000 <jatinrawatrawat2000@gamail.com>
2023-06-09 09:59:00 +0530

    my project

---
+++ obbs/admin/AFFORDABLE_PLACES_CATERS.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Table with database</title>
+<style>
+table {
+border-collapse: collapse;
+width: 100%;
+color: #588c7e;
+font-family: monospace;
+font-size: 25px;
+text-align: left;
+}
+th {
+background-color: #588c7e;
+color: white;
+}
+tr:nth-child(even) {background-color: #f2f2f2}
+</style>
+</head>
+<body>
+<table>
+<tr>
+<th>Id</th>
+<th>SERVICE NAME</th>
+<th>SERVICE PRICE</th>
+</tr>
+<?php
+$conn = mysqli_connect("localhost", "root", "", "obbs");
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+$sql = "SELECT id, ServiceName,ServicePrice  from tblservice where ServicePrice <20000";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+// output data of each row
+while($row = $result->fetch_assoc()) {
+echo "<tr><td>" . $row["id"]. "</td><td>" . $row["ServiceName"] . "</td><td>"
+. $row["ServicePrice"]. "</td></tr>";
+}
+echo "</table>";
+} else { echo "0 results"; }
+$conn->close();
+?>
+</table>
+</body>
+</html>
